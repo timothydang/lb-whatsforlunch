cf6232aace4de69ba728f9c64ccbafe923ddf6cf
Timothy Dang <timothy.dang@leoburnett.com.au>
2017-06-15 17:32:51 +1000

    Initial commit

---
+++ fuse.js
@@ -0,0 +1,57 @@
+const { FuseBox, CSSPlugin, SassPlugin, WebIndexPlugin, UglifyJSPlugin, Sparky } = require('fuse-box');
+
+let fuse, app, vendor, isProduction = false;
+
+Sparky.task('config', () => {
+    fuse = FuseBox.init({
+        homeDir: 'src',
+        output: 'dist/$name.js',
+        hash: isProduction,
+        sourceMaps: !isProduction,
+        plugins: [
+            [SassPlugin(), CSSPlugin()],
+            CSSPlugin(),
+            WebIndexPlugin({
+              template: 'src/index.html'
+            }),
+            isProduction && UglifyJSPlugin()
+        ]
+    });
+    // vendor should come first
+    vendor = fuse.bundle('vendor')
+        .instructions('~ index.tsx');
+
+    // out main bundle
+    app = fuse.bundle('app')
+        .instructions(`!> [index.tsx]`);
+
+    if (!isProduction) {
+        fuse.dev();
+    }
+});
+
+
+// development task 'node fuse''
+Sparky.task('default', ['config'], () => {
+    vendor.hmr().watch();
+    app.watch();
+    return fuse.run();
+});
+
+// Dist task 'node fuse dist'
+Sparky.task('dist', ['set-production', 'config'], () => {
+    return fuse.run();
+});
+
+
+Sparky.task('set-production', () => {
+    isProduction = true;
+    return Sparky.src('dist/').clean('dist/');
+});
+
+Sparky.task('test', ['config'], () => {
+    return app.test();
+});
+
+
+
